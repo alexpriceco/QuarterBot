60ffab1c08e9e8c773e2169d425b14b6b792e052
Alex Price <posixoddity@gmail.com>
2015-10-24 16:33:55 -0700

    Created secondary.php

---
+++ secondary.php
@@ -0,0 +1,18 @@
+<?php
+
+$year = 1;
+$major = $_POST['major'];
+
+// Major creation
+    //create arrays for each major l
+
+// Prereq function
+    // Receive major
+    // Match to pathway
+    // Check for first level courses
+    // IF no prereq, then respond with [0]
+    // ELSE, return ["prereq1name", "prereq2name", "etc"]
+
+// Pathway function
+
+?>
