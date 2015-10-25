22f2bcfd887d36589ea480b0b5f5ed4609c3cbd7
vitran9 <tran.victoria3@gmail.com>
2015-10-24 18:25:30 -0700

    Finished primary
    Created pre-req arrays and switch statement

--- php/secondary.php
+++ php/secondary.php
@@ -1,18 +1,51 @@
+//receives $apScores and returns class array with course type objects
+//y1q1[Course("$name", units), ..., ...,]
+
 <?php
+$regsFulfilled = $_POST['apscores']; //scores will be ints
 
-$year = 1;
-$major = $_POST['major'];
+//depending on these scores, the function should decide what classes that user should take
 
-// Major creation
-    //create arrays for each major l
+function getPath($major, $reqsFulfilled){
+	$cleanArray=RmvSP(getCourses($major), $reqsFulfilled);
+	$path=new array();
+	for ($c=0; $c<count($cleanArray);$c+=2)
+	{
+		array_push($path, array(NULL , NULL));  //need to fix
+	}
 
-// Prereq function
-    // Receive major
-    // Match to pathway
-    // Check for first level courses
-    // IF no prereq, then respond with [0]
-    // ELSE, return ["prereq1name", "prereq2name", "etc"]
+	$q=0;
+	$c=0;
+	while(count($cleanArray)>0){
+		foreach($cleanArray as $x){ // $x=each object in $cleanArray
+			if(count($x->$prereqs)==0){
+				$path[q][c]=$x;
+				if ($c==0){       //c and q define position in the $path 2darray
+					$c++;
+				}
+				else{
+					$c=0;
+					q++;
+				}
+				foreach($cleanArray as $y){
+					for($z=0; $z<count($y->$prereqs); $z++){
+						if ($y->$prereqs[$z]==$x){
+							unset($y->$prereqs[$z]);
+							$z--;
+						}
+					}
+				}
+				unset($x);
+				break;
+			}
+		}
+	return $path;
+  }
+}
 
-// Pathway function
+
+
+
+
 
 ?>
