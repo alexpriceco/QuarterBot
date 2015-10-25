e8de787dbba76285114fadb0f5ed3a3e4dfcd8a8
Alex Price <posixoddity@gmail.com>
2015-10-24 23:09:26 -0700

    Fixed php
    Some broken shit up in there

--- php/primary.php
+++ php/primary.php
@@ -10,46 +10,42 @@ $cs = new array();
 $psych = new array();
 
 class Test{
-  $testName;
-  $minScore;
-  $maxScore;
+    $testName;
+    $minScore;
+    $maxScore;
 
-  public function __construct($testName, $minScore, $maxScore){
-    this->testName = $testName;
-    this->minScore = $minScore;
-    this->maxScore = $maxScore;
-  }
-  // Possibly applicable apScores for pre-req eval
-  $apcsTest = new Test("APCS", 0, 5);
-  $apCalcAB = new Test("APCALCAB", 0, 5);
-  $apCalcBC = new Test("APCALCBC", 0, 5);
-  $apPyschTest = new Test("APPYSCH", 0, 5);
+    public function __construct($testName, $minScore, $maxScore){
+        this->testName = $testName;
+        this->minScore = $minScore;
+        this->maxScore = $maxScore;
+    }
+    
+    // Possibly applicable apScores for pre-req eval
+    $apcsTest = new Test("APCS", 0, 5);
+    $apCalcAB = new Test("APCALCAB", 0, 5);
+    $apCalcBC = new Test("APCALCBC", 0, 5);
+    $apPyschTest = new Test("APPYSCH", 0, 5);
 
-  // Push all the values into the $cs and $psych libraries
-  array_push($cs, $apcsTest);
-  array_push($cs, $apCalcABTest);
-  array_push($cs, $apCalcBCTest);
-  array_push($psych, $apPyschTest);
+    // Push all the values into the $cs and $psych libraries
+    array_push($cs, $apcsTest);
+    array_push($cs, $apCalcABTest);
+    array_push($cs, $apCalcBCTest);
+    array_push($psych, $apPyschTest);
 }
 
 // Switch case used for scalability in the future
-<<<<<<< HEAD
 switch ($major){
-  case ("cs"){
-    return $cs;
-  }
-  case ("psych"){
-    return $psych;
-  }
-  default{
-    return "sorry";
-  }
+  case ("cs"):
+      $output =  $cs;
+      break;
+  case ("psych"):
+      $output =  $psych;
+      break;
+  default:
+      $output =  "sorry";
+      break;
 }
-=======
-switch ($major):
-  case ("cs") : return $cs;
-  case ("psych") : return $psych;
-endswitch;
->>>>>>> origin/master
 
-  ?>
+echo $output;
+
+?>
